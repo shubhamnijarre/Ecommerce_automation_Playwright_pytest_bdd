8e4906479108881da77a46c558d3af49188b0b25
Yourproject <foxwin.00@gmail.com>
2025-12-26 18:47:31 +0530

    Adding docker file

--- Pages/Product_page.py
+++ Pages/Product_page.py
@@ -3,18 +3,35 @@ from Locators.Product_locators import product_locators
 from Pages.Helper import Helper1
 import time
 import pytest_check as check
-class product_page:
+class product_page(Helper1):
 
         @staticmethod
         def click_add_to_cart(page:Page,expect:Expect):
+                 
+                 addtocart_locator_tuple=product_locators.product_page["Addtocart_button"]
+                #   time.sleep(10)
+                  
+                 
+                #   page.locator().dblclick()
+                #   time.sleep(10)
+                 Helper1.getelements(page,addtocart_locator_tuple).click(force=True)
+                 page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added","Product added") )
+                 page.on("dialog", lambda dialog:print(f"messgae{dialog.message}"))
+               #   with page.expect_popup() as f:
+               #     Helper1.getelements(page,addtocart_locator_tuple).click(force=True)
+               #     time.sleep(10)
+               #     dialog=f.value
+               #     print(dialog)
+               #     page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added","Product added") )
+               #     page.on("dialog", lambda dialog:print(f"messgae{dialog.message}"))
+                 print("Clicked on add to cart button")
+               #   time.sleep(15)
+           
           
-         addtocart_locator_tuple=product_locators.product_page["Addtocart_button"]
-         
-         Helper1.getelements(page,addtocart_locator_tuple).click()
-         print("Clicked on add to cart button")
+          
    
         #  print(page.context.cookies())
-        #  time.sleep(5)
+     #     time.sleep(15)
          
          
          
@@ -22,15 +39,25 @@ class product_page:
         
 
 
-
+        @staticmethod
         def popup_message(page:Page):
-             print("i am in popup message method")
-             addtocart_locator_tuple=product_locators.product_page["Addtocart_button"]
+                     print("i am in popup message method")
+                 
+                     addtocart_locator_tuple=product_locators.product_page["Addtocart_button"]
+                     page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added","Product added") )
+                     page.on("dialog", lambda dialog:print(f"messgae{dialog.message}"))
+               #   with page.expect_popup(page) as f:
+                         
+               #       Helper1.getelements(page,addtocart_locator_tuple).click()
+               #       page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added","Product added") )
+               #       page.on("dialog", lambda dialog:print(f"messgae{dialog.message}") )
+                 
              
-            #  time.sleep(5)
+          #    time.sleep(15)
+          #   Helper1.getelements(page,popup_message.)
             
-             page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added.","Product added") )
-             page.on("dialog", lambda dialog: print(f"Dialog message: {dialog.message}"))
+          #     page.on("dialog",lambda dialog: check.is_true(dialog.message=="Product added","Product added") )
+          #     page.on("dialog", lambda dialog:print(f"messgae{dialog.message}") )
              
             #  with page.expect_event("dialog") as dialog_info:
             #      Helper1.getelements(page,addtocart_locator_tuple).click()
